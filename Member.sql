eeb323482f64a0dba0497347bc43f79ee93fe86c
COMA91 <121143062+COMA91@users.noreply.github.com>
2022-12-21 11:54:38 +0900

    Add files via upload

---
+++ Member.sql
@@ -0,0 +1,14 @@
+CREATE TABLE tbl_member(
+id INT NOT NULL,
+pw VARCHAR(15) NOT NULL,
+name VARCHAR2(15) NOT NULL,
+mail VARCHAR2(100) NOT NULL
+phone NUMBER(11) NOT NULL,
+birth 
+addr CHAR(25) NOT NULL,
+sex 
+age NUMBER(3) NOT NULL
+grade
+);
+
+
